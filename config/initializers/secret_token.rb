f5f2808ab6153d39de0ad89e5f01f3ae4ba942d7
Bernard Banta <banta.bernard@gmail.com>
2012-04-29 00:59:30 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epuka::Application.config.secret_token = '8449e49b8135e3f82d60046b20b8d809e421ee28e51799cfe65d4a46cec8a7c082a6e7ed0e5c05a5d4a9a668f5b15155572aedbf942de658097c8a2178a60c33'
