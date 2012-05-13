2277f2f0db2199683aedb084c2ec9224b9395fe7
Bernard Banta <banta.bernard@gmail.com>
2012-05-13 17:06:03 +0300

    compiled assets

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120513103053) do
 
-  create_table "blogs", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "contacts", :force => true do |t|
     t.string   "name"
     t.string   "email"
